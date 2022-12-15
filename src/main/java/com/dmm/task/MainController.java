c48ccf9b9a1fd6b6488af633efe3dc92594ad30c
YamadaYukiko <78962956+yahiro0517@users.noreply.github.com>
2022-12-15 09:27:51 +0900

    カレンダー作成中

--- src/main/java/com/dmm/task/MainController.java
+++ src/main/java/com/dmm/task/MainController.java
@@ -1,12 +1,49 @@
 package com.dmm.task;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.dmm.task.data.entity.Tasks;
+import com.dmm.task.data.repository.TaskRepository;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	private TaskRepository repo;
+	
+	/**
+	 * カレンダーを作成
+	 * @param model モデル
+	 * @return 遷移先
+	 */
 	@GetMapping("/main")
-	String main() {
-		return "main";
+	public String main(Model model) {
+		// 1週間分のリスト
+		List<List<Tasks>> weeks = new ArrayList<>();
+			weeks.addAll(Collections.nCopies(7, null));			
+		
+		// その月の1日のLocalDateを取得
+		LocalDate d1 = LocalDate.now().withDayOfMonth(1);
+		// 曜日を取得
+		DayOfWeek week = d1.getDayOfWeek();
+		// マイナスして前月分のLocalDateを求める
+		LocalDate lastM = d1.minusMonths(1);
+		
+		
+		
+		
+		return "/main";
+		
+		
+		
 	}
 }
--- src/main/java/com/dmm/task/config/WebSecurityConfig.java
+++ src/main/java/com/dmm/task/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -48,6 +49,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// ログアウト設定
 		http.logout().logoutSuccessUrl("/loginForm") // ログアウト成功時に遷移するパス
 				.permitAll(); // 全ユーザに対して許可
+	}
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	    web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/img/**", "/icon/**");
 	}
 
 }
